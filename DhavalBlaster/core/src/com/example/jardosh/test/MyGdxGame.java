feb2fc702af4beedcc98469d4da73beabd24893b
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-01-18 16:22:06 -0600

    asd

--- DhavalBlaster/core/src/com/example/jardosh/test/MyGdxGame.java
+++ DhavalBlaster/core/src/com/example/jardosh/test/MyGdxGame.java
@@ -5,39 +5,42 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
-public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
+public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener {
 
     private SpriteBatch batch;
-    private BitmapFont font;
-    private int screenWidth, screenHeight;
-    private String message = "Touch Me!";
+    private OrthographicCamera camera;
+    private Texture texture;
+    private Sprite sprite;
 
 	@Override
 	public void create () {
         batch = new SpriteBatch();
-        screenWidth = Gdx.graphics.getWidth();
-        screenHeight = Gdx.graphics.getHeight();
 
-        font = new BitmapFont();
-        font.setColor(Color.FIREBRICK);
-        font.getData().scale(5);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        texture = new Texture(Gdx.files.internal("map.png"));
+        sprite = new Sprite(texture);
 
-        Gdx.input.setInputProcessor(this);
+        sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 
+        Gdx.input.setInputProcessor(new GestureDetector(this));
 	}
 
     @Override
     public void dispose() {
         batch.dispose();
-        font.dispose();
+        texture.dispose();
     }
 
 	@Override
@@ -45,58 +48,57 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
         Gdx.gl.glClearColor(0,1,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        GlyphLayout layout = new GlyphLayout();
-        layout.setText(font,message);
-
-        float height = screenHeight/2 - layout.height/2;
-        float width = screenWidth/2 - layout.width/2;
+        batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        font.draw(batch,message, width,height);
+        sprite.draw(batch);
         batch.end();
 	}
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-	    message = "Touch down at " + screenX + " + " +screenY;
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+	    camera.translate(-deltaX,deltaY);
+	    camera.update();
         return true;
     }
 
     @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        message = "Touch UP at " + screenX + " + " +screenY;
-        return true;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        message = "Dragging at " + screenX + " + " +screenY;
+    public boolean touchDown(float x, float y, int pointer, int button) {
         return false;
     }
 
     @Override
-    public boolean keyDown(int keycode) {
+    public boolean tap(float x, float y, int count, int button) {
         return false;
     }
 
     @Override
-    public boolean keyUp(int keycode) {
+    public boolean longPress(float x, float y) {
         return false;
     }
 
     @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
+    public boolean fling(float velocityX, float velocityY, int button) {
         return false;
     }
 
     @Override
-    public boolean scrolled(int amount) {
+    public boolean panStop(float x, float y, int pointer, int button) {
         return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+
     }
 }
