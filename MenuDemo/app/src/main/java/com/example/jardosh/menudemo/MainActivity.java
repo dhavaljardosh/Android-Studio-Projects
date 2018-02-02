50fe33daa6c1c92df554b051e786be866b88c24f
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-02-02 10:55:54 -0600

    Menu Demo

---
+++ MenuDemo/app/src/main/java/com/example/jardosh/menudemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.jardosh.menudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(),"Settings Selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.help:
+                Toast.makeText(getApplicationContext(),"Help Selected",Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
