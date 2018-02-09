9cdc381c3d078a47993ed372112982d51e1d29ea
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-02-09 09:47:15 -0600

    Image Sharing Done

--- Instagram/ParseStarterProject/src/main/java/com/parse/starter/PostLogin.java
+++ Instagram/ParseStarterProject/src/main/java/com/parse/starter/PostLogin.java
@@ -2,8 +2,11 @@ package com.parse.starter;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,12 +14,19 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,6 +49,17 @@ public class PostLogin extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1) {
+            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+                getPhoto();
+            }
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.share) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -48,6 +69,8 @@ public class PostLogin extends AppCompatActivity {
                 } else {
                     getPhoto();
                 }
+            }else {
+                getPhoto();
             }
         }
 
@@ -83,5 +106,44 @@ public class PostLogin extends AppCompatActivity {
         });
         ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
         userListView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == RESULT_OK && data!= null){
+            Uri selectedImage = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),selectedImage);
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+
+                byte[] byteArray = stream.toByteArray();
+
+                ParseFile file = new ParseFile("image.png",byteArray);
+
+                ParseObject object = new ParseObject("Image");
+
+                object.put("image",file);
+                object.put("username",ParseUser.getCurrentUser().getUsername());
+                object.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            Toast.makeText(PostLogin.this,"Image Shared!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(PostLogin.this,"NOT STORED", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+//                Toast.makeText(getApplicationContext(),"Image Received",Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
