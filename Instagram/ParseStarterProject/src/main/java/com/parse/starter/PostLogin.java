9d00e458a03be8bc8b86936b622b1672847d1f3b
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-02-08 23:25:05 -0600

    Added image thing

--- Instagram/ParseStarterProject/src/main/java/com/parse/starter/PostLogin.java
+++ Instagram/ParseStarterProject/src/main/java/com/parse/starter/PostLogin.java
@@ -1,7 +1,14 @@
 package com.parse.starter;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,6 +21,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PostLogin extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.share_menu, menu);
+
+        return  super.onCreateOptionsMenu(menu);
+
+    }
+
+    public void getPhoto(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.share) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[] {
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE }, 1);
+                } else {
+                    getPhoto();
+                }
+            }
+        }
+
+        return  super.onOptionsItemSelected(item);
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
