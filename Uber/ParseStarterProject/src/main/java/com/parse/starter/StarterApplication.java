cdd8647c4f6b111c514b8c23bc5f9b166a807f9a
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-02-11 20:33:51 -0600

    Uber Started

---
+++ Uber/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("9bc0a7496179360486598d5eda62684224a28734")
+            .clientKey("7de53af8db6f86523dca16a784432819f7b9f1e6")
+            .server("http://18.219.147.7:80/parse/")
+            .build()
+    );
+
+    ParseObject object = new ParseObject("ExampleObject");
+    object.put("myNumber", "123");
+    object.put("myString", "Dhaval");
+
+    object.saveInBackground(new SaveCallback () {
+      @Override
+      public void done(ParseException ex) {
+        if (ex == null) {
+          Log.i("Parse Result", "Successful!");
+        } else {
+          Log.i("Parse Result", "Failed" + ex.toString());
+        }
+      }
+    });
+
+
+    ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
