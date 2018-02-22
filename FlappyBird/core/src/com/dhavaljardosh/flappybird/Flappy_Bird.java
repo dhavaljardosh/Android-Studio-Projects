4b73594e9531b64cedbe44ce4c4415b143ee281b
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-02-22 18:53:39 +0530

    tubes added

--- FlappyBird/core/src/com/dhavaljardosh/flappybird/Flappy_Bird.java
+++ FlappyBird/core/src/com/dhavaljardosh/flappybird/Flappy_Bird.java
@@ -17,6 +17,9 @@ public class Flappy_Bird extends ApplicationAdapter {
 	int velocity = 0;
 	int gameState = 0;
 	float gravity = 2;
+	Texture topTube;
+	Texture bottomTube;
+	float gap = 400;
 	
 	@Override
 	public void create () {
@@ -26,24 +29,38 @@ public class Flappy_Bird extends ApplicationAdapter {
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
 		birdY = Gdx.graphics.getHeight()/2-(birds[0].getHeight()/2);
+
+		topTube = new Texture("toptube.png");
+		bottomTube = new Texture("bottomtube.png");
+
 	}
 
 	@Override
 	public void render () {
 
+		batch.begin();
+		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
 
 		if(gameState!=0) {
+
+			batch.draw(topTube,Gdx.graphics.getWidth()/2 - topTube.getWidth()/2,Gdx.graphics.getHeight()/2 + gap / 2);
+			batch.draw(bottomTube,Gdx.graphics.getWidth()/2 - bottomTube.getWidth()/2,Gdx.graphics.getHeight()/2 - gap/2 - bottomTube.getHeight());
+
+
 			if(Gdx.input.justTouched()){
-				velocity = -20;
+				velocity = -30;
 			}
 
-			velocity= (int) (velocity+gravity);
-			birdY -= velocity;
+
+			if(birdY>0 || velocity<0){
+				velocity= (int) (velocity+gravity);
+				birdY -= velocity;
+			}
 
 		} else{
 			if(Gdx.input.justTouched()){
-				gameState=1;
+				gameState = 1;
 			}
 		}
 
@@ -56,8 +73,7 @@ public class Flappy_Bird extends ApplicationAdapter {
 		}
 
 
-		batch.begin();
-		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
 		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - (birds[flapState].getWidth() / 2), birdY);
 		batch.end();
 
