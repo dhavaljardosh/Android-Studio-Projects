3f424f74bf805637397687333be5125ca187c1f8
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-04-12 10:16:00 -0500

    Completed

--- Twitter/ParseStarterProject/src/main/java/com/parse/starter/Main2Activity.java
+++ Twitter/ParseStarterProject/src/main/java/com/parse/starter/Main2Activity.java
@@ -94,6 +94,9 @@ public class Main2Activity extends AppCompatActivity {
 
             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(intent);
+        } else if(item.getItemId() == R.id.viewFeed){
+            Intent intent = new Intent(getApplicationContext(),Feed.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
